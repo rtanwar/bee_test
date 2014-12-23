2658a1f141a36101931f7ff788a433f75911171b
rahul <rt@RT-PC-HOME.(none)>
2014-12-23 06:45:34 +0530

    country db

--- controllers/default.go
+++ controllers/default.go
@@ -25,7 +25,7 @@ func (c *MainController) Get() {
 		// this.SetSession("asta", int(1))
 		// this.Data["num"] = 0
 	}
-	c.Data["Countries"] = models.GetAllCountry()
+	c.Data["Countries"], _ = models.GetAllCountry()
 	fmt.Println(c.Data["Countries"])
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/rtanwar/bee_test/routers"
 )
 
+func init() {
+	orm.RegisterDriver("mysql", orm.DR_MySQL)
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/world")
+}
 func main() {
 	// beego.SetStaticPath("/static", "public")
+
 	beego.Run()
 }
